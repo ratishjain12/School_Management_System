27efcaf547b9d5b9332a4bce2a82a7b0c70cad21
Nerdity <50540961+Nerdity@users.noreply.github.com>
2020-06-11 20:10:58 +0530

    minor changes

--- school project/attendence.py
+++ school project/attendence.py
@@ -1,8 +1,9 @@
 from tkinter import *
 
-gui = Tk()
-gui.geometry('500x500')
-gui.resizable(0,0)
+attendenceGui = Tk()
+attendenceGui.geometry('500x500')
+attendenceGui.title('Attendence Report')
+attendenceGui.resizable(0,0)
 
 
 
@@ -15,22 +16,22 @@ def submit():
 
 
 
-defaultDate = StringVar(gui)
+defaultDate = StringVar(attendenceGui)
 defaultDate.set('1')
-defaultMonth = StringVar(gui)
+defaultMonth = StringVar(attendenceGui)
 defaultMonth.set('January')
 var = IntVar()
 labelDate = Label(text = 'Select Date')
 labelMonth = Label(text = 'Select Month')
-dateMenu = OptionMenu(gui, defaultDate, '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31')
-monthMenu = OptionMenu(gui, defaultMonth, 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
-presentButton = Radiobutton(gui, text = 'Present', variable = var, value = 'present')
-absentButton = Radiobutton(gui, text = 'Absent', variable = var, value = 'absent')
-submitButton = Button(gui, text = 'Submit', command = submit)
-studentIdEntry = Entry(gui)
-studentIdLabel = Label(gui, text = 'Enter Student ID')
-studentNameEntry = Entry(gui, state = 'disabled')
-studentNameLabel = Label(gui, text = 'Student Name')
+dateMenu = OptionMenu(attendenceGui, defaultDate, '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31')
+monthMenu = OptionMenu(attendenceGui, defaultMonth, 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
+presentButton = Radiobutton(attendenceGui, text = 'Present', variable = var, value = 'present')
+absentButton = Radiobutton(attendenceGui, text = 'Absent', variable = var, value = 'absent')
+submitButton = Button(attendenceGui, text = 'Submit', command = submit)
+studentIdEntry = Entry(attendenceGui)
+studentIdLabel = Label(attendenceGui, text = 'Enter Student ID')
+studentNameEntry = Entry(attendenceGui, state = 'disabled')
+studentNameLabel = Label(attendenceGui, text = 'Student Name')
 
 
 labelDate.place(rely = 0.3, relx = 0.35)
@@ -46,4 +47,4 @@ studentNameEntry.place(rely = 0.19, relx = 0.45)
 studentNameLabel.place(rely = 0.19, relx = 0.25)
 
 
-gui.mainloop()
+attendenceGui.mainloop()
